3191f7553ec4e8742e371c1163717ed59b98c150
Artem Gromakov <gromakov.art3m@gmail.com>
2024-02-29 23:40:23 +0300

    Resolved an issue with json naming in github dto's

--- scrapper/src/main/java/edu/scrapper/client/github/dto/IssueTo.java
+++ scrapper/src/main/java/edu/scrapper/client/github/dto/IssueTo.java
@@ -1,5 +1,9 @@
 package edu.scrapper.client.github.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public record IssueTo(
     String title,
     UserTo user,
--- scrapper/src/main/java/edu/scrapper/client/github/dto/PullTo.java
+++ scrapper/src/main/java/edu/scrapper/client/github/dto/PullTo.java
@@ -1,5 +1,9 @@
 package edu.scrapper.client.github.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public record PullTo(
     Integer id,
     String title,
