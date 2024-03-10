33033a5e1a616d59477f7c30bbfa3bfa36b49862
Artem Gromakov <gromakov.art3m@gmail.com>
2024-03-10 23:28:38 +0300

    Created tables

---
+++ migrations/v0-initial.sql
@@ -0,0 +1,20 @@
+--liquibase formatted sql
+--changeset unters:1
+
+CREATE TYPE chat_status
+AS ENUM ('awaiting_command', 'awaiting_url_to_track', 'awaiting_url_to_untrack');
+
+CREATE TABLE chat (
+    id              bigint      PRIMARY KEY,
+    status          chat_status NOT NULL DEFAULT 'awaiting_command'
+);
+
+CREATE TABLE link (
+    id              serial      PRIMARY KEY,
+    url             bigint      NOT NULL,
+    updated_at      timestamp   NOT NULL DEFAULT now(),
+    chat_id         bigint      REFERENCES chat(id)
+);
+
+ALTER TABLE link
+ADD CONSTRAINT unique_chat_urls UNIQUE (url, chat_id);
